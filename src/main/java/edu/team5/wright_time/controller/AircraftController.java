4b7bed6ed92f4f8aaaa111b312101ef5f2bd0b0a
EmbodyTheLogos <EmbodyTheLogos@users.noreply.github.com>
2020-09-24 18:42:23 -0400

    Update gitignore

--- src/main/java/edu/team5/wright_time/controller/AircraftController.java
+++ src/main/java/edu/team5/wright_time/controller/AircraftController.java
@@ -3,12 +3,14 @@ package edu.team5.wright_time.controller;
 import edu.team5.wright_time.model.entity.Aircraft;
 import edu.team5.wright_time.model.repository.AircraftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/aircraft")
+@CrossOrigin(origins = "http://localhost:3030")
 public class AircraftController {
     private AircraftRepository aircraftRepository;
     @Autowired
