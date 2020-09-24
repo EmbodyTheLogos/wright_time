bc884290fecd124158ee9b322052710b18b08cd6
EmbodyTheLogos <EmbodyTheLogos@users.noreply.github.com>
2020-09-24 17:38:04 -0400

    Initial commit

---
+++ src/main/java/edu/team5/wright_time/WrightTimeApplication.java
@@ -0,0 +1,13 @@
+package edu.team5.wright_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrightTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrightTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/team5/wright_time/WrightTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.team5.wright_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrightTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
