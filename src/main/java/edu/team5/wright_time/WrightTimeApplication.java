d1eac7f5f49dde4862c88da7cb0a88db804e8758
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-09-24 18:31:27 -0400

    Suppressed Spring Security for testing purposes

--- src/main/java/edu/team5/wright_time/WrightTimeApplication.java
+++ src/main/java/edu/team5/wright_time/WrightTimeApplication.java
@@ -2,8 +2,9 @@ package edu.team5.wright_time;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WrightTimeApplication {
 
     public static void main(String[] args) {
