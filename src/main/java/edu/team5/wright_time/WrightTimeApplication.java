93c72dcac878717b5c8f567cb42d04c7522c6685
rustylocks79 <jeremydellock570@gmail.com>
2020-09-24 18:37:49 -0400

    Bug fix

--- src/main/java/edu/team5/wright_time/WrightTimeApplication.java
+++ src/main/java/edu/team5/wright_time/WrightTimeApplication.java
@@ -1,14 +1,26 @@
 package edu.team5.wright_time;
 
+import edu.team5.wright_time.model.entity.Aircraft;
+import edu.team5.wright_time.model.repository.AircraftRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WrightTimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WrightTimeApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(AircraftRepository repository) {
+        return (args) -> {
+            repository.save(new Aircraft("A", "B"));
+            repository.save(new Aircraft("C", "D"));
+        };
     }
 
 }
--- src/main/java/edu/team5/wright_time/controller/AircraftController.java
+++ src/main/java/edu/team5/wright_time/controller/AircraftController.java
@@ -3,6 +3,7 @@ package edu.team5.wright_time.controller;
 import edu.team5.wright_time.model.entity.Aircraft;
 import edu.team5.wright_time.model.repository.AircraftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ public class AircraftController {
     public AircraftController(AircraftRepository aircraftRepository) {
         this.aircraftRepository = aircraftRepository;
     }
+
+    @GetMapping
     public Iterable<Aircraft> getAircraft(){
 
         return aircraftRepository.findAll();
