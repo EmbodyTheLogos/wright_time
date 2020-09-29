dea68cfa0b6add9c1b76547e955b2eb80420445b
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-09-29 17:22:47 -0400

    finished initial setup to view database

---
+++ frontend/src/components/AircraftComponent.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import AircraftService from '../services/AircraftService';
+
+class AircraftComponent extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            aircrafts:[]
+        }
+    }
+
+    componentDidMount(){
+        AircraftService.getAircrafts().then((response) => {
+            this.setState({ aircrafts: response.data})
+        });
+    }
+
+    render (){
+        return (
+            <div>
+                <h1 className = "text-center"> Aircraft List</h1>
+                <table className = "table table-striped">
+                    <thead>
+
+                    <tr>
+                        <td> User Id</td>
+                        <td> User Manufacturer</td>
+                        <td> User Model</td>
+                    </tr>
+
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.aircrafts.map(
+                            aircraft =>
+                                <tr key = {aircraft.aircraftId}>
+                                    <td> {aircraft.aircraftId}</td>
+                                    <td> {aircraft.manufacturer}</td>
+                                    <td> {aircraft.model}</td>
+                                </tr>
+                        )
+                    }
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        )
+    }
+}
+
+export default AircraftComponent
--- frontend/src/services/AircraftService.js
+++ frontend/src/services/AircraftService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const AIRCRAFT_REST_API_URL = 'http://localhost:8080/api/aircraft';
 
 class AircraftService {
-    getUsers(){
+    getAircrafts(){
         return axios.get(AIRCRAFT_REST_API_URL);
     }
 }
