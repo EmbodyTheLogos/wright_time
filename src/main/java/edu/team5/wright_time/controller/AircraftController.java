ae8b21936d8618410c707cf0ad996b981420b3a0
EmbodyTheLogos <EmbodyTheLogos@users.noreply.github.com>
2020-10-02 13:54:30 -0400

    New methods added: -add a new Aircraft -get a single Aircraft -update an Aircraft -delete an Aircraft

--- src/main/java/edu/team5/wright_time/controller/AircraftController.java
+++ src/main/java/edu/team5/wright_time/controller/AircraftController.java
@@ -3,16 +3,15 @@ package edu.team5.wright_time.controller;
 import edu.team5.wright_time.model.entity.Aircraft;
 import edu.team5.wright_time.model.repository.AircraftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/aircraft")
 @CrossOrigin(origins = "http://localhost:3000")
 public class AircraftController {
     private AircraftRepository aircraftRepository;
+    private Aircraft aircraft;
+
     @Autowired
     public AircraftController(AircraftRepository aircraftRepository) {
         this.aircraftRepository = aircraftRepository;
@@ -23,4 +22,31 @@ public class AircraftController {
 
         return aircraftRepository.findAll();
     }
+
+    @PostMapping
+    public void addAircraft(Aircraft aircraft)
+    {
+        aircraftRepository.save(aircraft);
+    }
+
+    @GetMapping("api/aircraft/{id}")
+    public Aircraft getOneAircraft(Integer id)
+    {
+       return aircraftRepository.findById(id).get();
+    }
+
+
+    @PostMapping("api/aircraft/{id}")
+    public void updateAircraft(Aircraft aircraft)
+    {
+        aircraftRepository.save(aircraft);
+    }
+
+
+    @PostMapping("api/aircraft/{id}")
+    public void deleteAircraft(Integer id)
+    {
+        aircraftRepository.delete(getOneAircraft(id));
+    }
+
 }
