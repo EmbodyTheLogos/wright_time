b1de63547288ee5e99ea963ee9cd2ee39f30ee0e
rustylocks79 <jeremydellock570@gmail.com>
2020-10-07 16:05:42 -0400

    updated AircraftService.js

--- frontend/src/services/AircraftService.js
+++ frontend/src/services/AircraftService.js
@@ -7,8 +7,20 @@ class AircraftService {
         return axios.get(AIRCRAFT_REST_API_URL);
     }
 
+    getOneAircraft(id) {
+        return axios.get(AIRCRAFT_REST_API_URL + '/' + id)
+    }
+
     postAircraft(aircraft) {
         return axios.post(AIRCRAFT_REST_API_URL, aircraft)
+    }
+
+    putAircraft(id, aircraft) {
+        return axios.put(AIRCRAFT_REST_API_URL + '/' + id, aircraft)
+    }
+
+    deleteAircraft(id) {
+        return axios.delete(AIRCRAFT_REST_API_URL + '/' + id)
     }
 }
 
