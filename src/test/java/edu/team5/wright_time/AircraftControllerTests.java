bc60d4faaaab4cea10f82dea3c3736effc9e0633
EmbodyTheLogos <EmbodyTheLogos@users.noreply.github.com>
2020-10-11 22:39:14 -0400

    New Test Methods: Add and Delete. Seems like it working but not sure.

--- src/test/java/edu/team5/wright_time/AircraftControllerTests.java
+++ src/test/java/edu/team5/wright_time/AircraftControllerTests.java
@@ -1,5 +1,6 @@
 package edu.team5.wright_time;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.team5.wright_time.controller.AircraftController;
 import edu.team5.wright_time.model.entity.Aircraft;
 import org.hamcrest.Matchers;
@@ -9,14 +10,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +27,8 @@ class AircraftControllerTests {
 
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private AircraftController controller;
@@ -50,6 +55,32 @@ class AircraftControllerTests {
                 .andExpect(jsonPath("$[1].year", Matchers.equalTo(2000)))
                 .andExpect(jsonPath("$[1].maintenanceDay", Matchers.equalTo(3)))
                 .andExpect(jsonPath("$[1].minimumTrainingDuration", Matchers.equalTo(3)));
+
+    }
+
+    //https://reflectoring.io/spring-boot-web-controller-test/
+    @Test
+    void testAddAircraft() throws Exception {
+        var aircraft3 = new Aircraft("Man3", "Nam3", "Mod3", 2001, 4, 5);
+        this.mvc.perform(post("/api/aircraft")
+                .contentType("application/json")
+                .param("aircraft3.getManufacturer()", "Man3")
+                .param("aircraft3.getName()", "Nam3")
+                .param("aircraft3.getModel()", "Mod3")
+                .param("aircraft3.getYear()", "2001")
+                .param("aircraft3.getMaintenanceDay()", "4")
+                .param("aircraft3.getMinimumTrainingDuration()", "5")
+                .content(objectMapper.writeValueAsString(aircraft3)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteAircraft() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders
+                .delete("/api/aircraft/{id}", "0")
+                .contentType("application/json")
+                .accept("application/json"))
+            .andExpect(status().isOk());
     }
 
 }
