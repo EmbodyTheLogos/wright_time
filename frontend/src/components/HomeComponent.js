37e05f79cfda88f357c9b378863c020533307045
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-10-25 17:21:27 -0400

    made HomeComponent.js look much better, with more information

--- frontend/src/components/HomeComponent.js
+++ frontend/src/components/HomeComponent.js
@@ -14,17 +14,52 @@ class HomeComponent extends React.Component {
     render() {
         return (
             <div>
-                <Link to="/aircraft">
-                    <button type="button">
-                        Aircraft
-                    </button>
-                </Link>
 
-                <Link to="/users">
-                    <button type="button">
-                        Users
-                    </button>
-                </Link>
+                <div className="container mt-4">
+                    <h3>Database Contents</h3>
+                    <table className="table table-bordered table-hover">
+                        <thead className="thead-dark">
+                        <tr>
+                            <th scope="col" width="150">Type</th>
+                            <th scope="col" width="150">Students</th>
+                            <th scope="col" width="150">Instructors</th>
+                            <th scope="col" width="150">Administrators</th>
+                            <th scope="col" width="150">Link</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class={"table-primary"}>
+                            <td>Aircraft</td>
+                            <td>View</td>
+                            <td>View</td>
+                            <td>Edit, View</td>
+                            <td><a href={"/aircraft"} className={"btn btn-secondary btn-block"}>Check it out!</a></td>
+                        </tr>
+                        <tr class={"table-info"}>
+                            <td>Users</td>
+                            <td>No Access</td>
+                            <td>No Access</td>
+                            <td>Edit, View</td>
+                            <td><a href={"/users"} className={"btn btn-secondary btn-block"}>Check it out!</a></td>
+                        </tr>
+                        <tr class={"table-warning"}>
+                            <td>Sessions</td>
+                            <td>Limited View</td>
+                            <td>Limited View</td>
+                            <td>Edit, View</td>
+                            <td><a href={"/aircraft"} className={"btn btn-secondary btn-block"}>Check it out!</a></td>
+                        </tr>
+                        <tr class={"table-danger"}>
+                            <td>Certifications</td>
+                            <td>Limited View</td>
+                            <td>Limited View</td>
+                            <td>Edit, View</td>
+                            <td><a href={"/aircraft"} className={"btn btn-secondary btn-block"}>Check it out!</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         )
     }
