2c4415805c17c0bd321bec5a3f16eb57aab1e046
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-10-28 17:45:29 -0400

    updated HomeComponent.js

--- frontend/src/components/HomeComponent.js
+++ frontend/src/components/HomeComponent.js
@@ -26,7 +26,7 @@ class HomeComponent extends React.Component {
                 </Navbar>
 
                 <div className="container mt-4">
-                    <h3>Database Contents</h3>
+                    <h3>Database Tables Details</h3>
                     <table className="table table-bordered table-hover">
                         <thead className="thead-dark">
                         <tr>
@@ -34,7 +34,7 @@ class HomeComponent extends React.Component {
                             <th scope="col" width="150">Students</th>
                             <th scope="col" width="150">Instructors</th>
                             <th scope="col" width="150">Administrators</th>
-                            <th scope="col" width="150">Link</th>
+                            {/*<th scope="col" width="150">Link</th>*/}
                         </tr>
                         </thead>
                         <tbody>
@@ -43,31 +43,31 @@ class HomeComponent extends React.Component {
                             <td>View</td>
                             <td>View</td>
                             <td>Edit, View</td>
-                            <td><Link to={"/aircraft"} className={"btn btn-secondary btn-block"}>Check it out!</Link>
-                            </td>
+                            {/*<td><Link to={"/aircraft"} className={"btn btn-secondary btn-block"}>Check it out!</Link>*/}
+                            {/*</td>*/}
                         </tr>
                         <tr className={"table-info"}>
                             <td>Users</td>
                             <td>No Access</td>
                             <td>No Access</td>
                             <td>Edit, View</td>
-                            <td><Link to={"/users"} className={"btn btn-secondary btn-block"}>Check it out!</Link></td>
+                            {/*<td><Link to={"/users"} className={"btn btn-secondary btn-block"}>Check it out!</Link></td>*/}
                         </tr>
                         <tr className={"table-warning"}>
                             <td>Sessions</td>
                             <td>Limited View</td>
                             <td>Limited View</td>
                             <td>Edit, View</td>
-                            <td><Link to={"/sessions"} className={"btn btn-secondary btn-block"}>Check it out!</Link>
-                            </td>
+                            {/*<td><Link to={"/sessions"} className={"btn btn-secondary btn-block"}>Check it out!</Link>*/}
+                            {/*</td>*/}
                         </tr>
                         <tr className={"table-danger"}>
                             <td>Certifications</td>
                             <td>Limited View</td>
                             <td>Limited View</td>
                             <td>Edit, View</td>
-                            <td><Link to={"/certifications"} className={"btn btn-secondary btn-block"}>Check it
-                                out!</Link></td>
+                            {/*<td><Link to={"/certifications"} className={"btn btn-secondary btn-block"}>Check it*/}
+                            {/*    out!</Link></td>*/}
                         </tr>
                         </tbody>
                     </table>
