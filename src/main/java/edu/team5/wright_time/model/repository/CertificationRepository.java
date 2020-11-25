f1ff247075743da837c5307d08f1471fa0c1943d
nhNetz <nathuntnet@yahoo.com>
2020-11-25 15:25:12 -0500

    Created get certification by aircraft method

--- src/main/java/edu/team5/wright_time/model/repository/CertificationRepository.java
+++ src/main/java/edu/team5/wright_time/model/repository/CertificationRepository.java
@@ -2,7 +2,11 @@ package edu.team5.wright_time.model.repository;
 
 import edu.team5.wright_time.model.entity.Certification;
 import org.springframework.data.repository.CrudRepository;
+import edu.team5.wright_time.model.entity.Aircraft;
+
+import java.util.List;
 
 public interface CertificationRepository extends CrudRepository <Certification, Long> {
 
+    List<Certification> findCertificationByAircraft(Aircraft aircraft);
 }
--- src/main/java/edu/team5/wright_time/model/repository/UserRepository.java
+++ src/main/java/edu/team5/wright_time/model/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package edu.team5.wright_time.model.repository;
 
 import edu.team5.wright_time.model.entity.User;
+import edu.team5.wright_time.model.entity.Certification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findUsersByRole(String role);
+
 }
