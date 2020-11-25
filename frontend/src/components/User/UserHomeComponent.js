b02e0aa6734e2b5275b9f34b7c5380e0e23ed85a
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-11-25 13:33:12 -0500

    placeholder user home component

--- frontend/src/components/User/UserHomeComponent.js
+++ frontend/src/components/User/UserHomeComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserNavbar from "../Navbars/UserNavbar";
+import {Col, Row, Container} from "react-bootstrap";
 
 class UserHomeComponent extends React.Component {
 
@@ -11,46 +12,119 @@ class UserHomeComponent extends React.Component {
         return (
             <div>
                 <UserNavbar/>
+                <br/>
 
-                <div className="container mt-4">
-                    <h3>Database Tables Details</h3>
-                    <table className="table table-bordered table-hover">
-                        <thead className="thead-dark">
-                        <tr>
-                            <th scope="col" width="150">Type</th>
-                            <th scope="col" width="150">Students</th>
-                            <th scope="col" width="150">Instructors</th>
-                            <th scope="col" width="150">Administrators</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr className={"table-primary"}>
-                            <td>Aircraft</td>
-                            <td>View</td>
-                            <td>View</td>
-                            <td>Edit, View</td>
-                        </tr>
-                        <tr className={"table-info"}>
-                            <td>Users</td>
-                            <td>No Access</td>
-                            <td>No Access</td>
-                            <td>Edit, View</td>
-                        </tr>
-                        <tr className={"table-warning"}>
-                            <td>Sessions</td>
-                            <td>Limited View</td>
-                            <td>Limited View</td>
-                            <td>Edit, View</td>
-                        </tr>
-                        <tr className={"table-danger"}>
-                            <td>Certifications</td>
-                            <td>Limited View</td>
-                            <td>Limited View</td>
-                            <td>Edit, View</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <Container>
+                    <Row>
+                        <Col md={7}>
+                            <Row>
+                                <table className="table table-bordered table-hover">
+                                    <thead className="thead-dark">
+                                    <tr>
+                                        <th scope="col" width="150">Calendar</th>
+                                        <th scope="col" width="150">Calendar</th>
+                                        <th scope="col" width="150">Calendar</th>
+                                        <th scope="col" width="150">Calendar</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                        <td>Calendar</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </Row>
+                            <br/>
+                            <Row className={"justify-content-md-center"}>
+                                Weekly Flight Hours: __Only God Knows__
+                            </Row>
+                        </Col>
+
+                        <Col md={1}> </Col>
+
+                        <Col md={4}>
+                            <Row>
+                                <table className="table table-bordered table-hover">
+                                    <thead className="thead-dark">
+                                    <tr>
+                                        <th scope="col" width="150">Upcoming Flights</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </Row>
+                            <Row>
+                                <table className="table table-bordered table-hover">
+                                    <thead className="thead-dark">
+                                    <tr>
+                                        <th scope="col" width="150">Recent Flights</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Placeholder</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
 
             </div>
         )
