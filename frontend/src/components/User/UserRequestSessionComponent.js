4a69554fe6712cdbab6d61f517b1bf511f91d9e2
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-12-07 22:56:47 -0500

    updated user request session form

--- frontend/src/components/User/UserRequestSessionComponent.js
+++ frontend/src/components/User/UserRequestSessionComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SessionService from '../../services/SessionService';
-import {Button, Container, Form} from 'react-bootstrap'
+import {Button, Col, Container, Form, Row} from 'react-bootstrap'
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,11 +9,16 @@ import UserService from "../../services/UserService";
 import UserNavbar from "../Navbars/UserNavbar";
 import {withCookies} from "react-cookie";
 import {withRouter} from "react-router-dom";
+import AircraftService from "../../services/AircraftService";
+import AdministratorNavbar from "../Navbars/AdministratorNavbar";
+import AuthService from "../../services/AuthService";
+import UserComponent from "../Administrator/UserComponent";
 
 class UserRequestSessionComponent extends React.Component {
     state = {
-        mode: "edit",
-        sessionId: -1,
+        mode: "",
+        id: -1,
+        sessionId: "",
         aircraftId: "",
         instructorId: "",
         studentId: "",
@@ -23,15 +28,24 @@ class UserRequestSessionComponent extends React.Component {
         score: "",
         comments: "",
         state: "",
-        users: [],
+        aircrafts: [],
+        students: [],
+        instructors: [],
         errorMessage: "",
-        jwtToken: ''
+        user: "",
+        jwtToken: ""
     };
 
     constructor(props){
         super(props)
         const {cookies} = props;
         this.state.jwtToken = cookies.get('JWT-TOKEN')
+        if (!props.match.params.id) {
+            this.state.mode = 'add'
+        } else {
+            this.state.mode = 'edit'
+            this.state.id = props.match.params.id
+        }
     }
 
     componentDidMount(){
@@ -55,8 +69,19 @@ class UserRequestSessionComponent extends React.Component {
                 });
             })
         }
-        UserService.getAll(this.state.jwtToken).then((response) => {
-            this.setState({ users: response.data})
+        UserService.getAllInstructors(this.state.jwtToken).then((response) => {
+            this.setState({ instructors: response.data})
+        })
+        UserService.getAllStudents(this.state.jwtToken).then((response) => {
+            this.setState({ students: response.data})
+        })
+        AircraftService.getAll(this.state.jwtToken).then((response) => {
+            this.setState({aircrafts: response.data})
+        })
+        AuthService.user(this.state.jwtToken).then((res) => {
+            this.setState({user: res.data})
+            this.setState({studentId: this.state.user.id})
+            //console.log(this.state.studentId)
         })
     }
 
@@ -89,7 +114,7 @@ class UserRequestSessionComponent extends React.Component {
 
         console.log(JSON.stringify(session));
         if(this.state.mode === "add") {
-            SessionService.post(session).then(res => {
+            SessionService.post(this.state.jwtToken, session).then(res => {
                 this.props.history.push('/admin/sessions')
             }).catch(res => {
                 if(res.response) {
@@ -99,7 +124,7 @@ class UserRequestSessionComponent extends React.Component {
                 }
             })
         } else {
-            SessionService.put(this.state.sessionId, session).then(res => {
+            SessionService.put(this.state.jwtToken, this.state.sessionId, session).then(res => {
                 this.props.history.push('/admin/sessions')
             }).catch(res => {
                 if(res.response) {
@@ -123,81 +148,94 @@ class UserRequestSessionComponent extends React.Component {
                 <Container>
                     <Center>
                         <Form>
-                            <Form.Group controlId={"aircraftId"}>
-                                <Form.Control type={"text"} placeholder={"Aircraft ID"}
-                                              value={this.state.aircraftId} onChange={this.changeHandler}
-                                              name={"aircraftId"}/>
+                            <Form.Group as={Row} controlId={"aircraftId"}>
+                                <Form.Label column sm={4}>Aircraft:</Form.Label>
+                                <Col sm={8}>
+                                    <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.aircraftId}
+                                                  onChange={this.changeHandler} name={"aircraftId"}>
+                                        <option value="empty"> </option>
+                                        {this.state.aircrafts.map(aircraft => <option key={aircraft.id} value={aircraft.id}>
+                                            {aircraft.manufacturer + " " + aircraft.model + " " + aircraft.name}</option>)}
+                                    </Form.Control>
+                                </Col>
                             </Form.Group>
 
-                            {/*<Form.Group controlId={"instructorId"}>*/}
-                            {/*    <Form.Control type={"text"} placeholder={"Instructor ID"}*/}
-                            {/*                  value={this.state.instructorId} onChange={this.changeHandler}*/}
-                            {/*                  name={"instructorId"}/>*/}
+                            <Form.Group as={Row} controlId={"instructorId"}>
+                                <Form.Label column sm={4}>Instructor:</Form.Label>
+                                <Col sm={8}>
+                                    <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.instructorId}
+                                                  onChange={this.changeHandler} name={"instructorId"}>
+                                        <option value="empty"> </option>
+                                        {this.state.instructors.map(instructor => <option key={instructor.id} value={instructor.id}>
+                                            {instructor.firstName + " " + instructor.lastName}</option>)}
+                                    </Form.Control>
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId={"studentId"}>
+                                <Form.Label column sm={4}>Student:</Form.Label>
+                                <Col sm={8}>
+                                    {/*<Form.Control as={"select"} className={"mr-sm-2"} value={this.state.studentId}*/}
+                                    {/*              onChange={this.changeHandler} name={"studentId"} plaintext readOnly>*/}
+                                    {/*    <option value="empty"> </option>*/}
+                                    {/*    {this.state.students.map(student => <option key={student.id} value={student.id}>*/}
+                                    {/*        {student.firstName + " " + student.lastName}</option>)}*/}
+                                    {/*</Form.Control>*/}
+                                    <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.studentId}
+                                                  onChange={this.changeHandler} name={"studentId"} plaintext readOnly>
+                                        {this.state.students.map(student => this.state.user.id === student.id && <option key={student.id} value={student.id}>
+                                            {student.firstName + " " + student.lastName}</option>)}
+                                    </Form.Control>
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId={"date"}>
+                                <Form.Label column sm={4}>Date:</Form.Label>
+                                <Col sm={8}>
+                                    <DatePicker
+                                        selected={this.state.date}
+                                        onChange={this.handleDateChange}
+                                        name="date"
+                                        dateFormat="MM/dd/yyyy"
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} controlId={"startTime"}>
+                                <Form.Label column sm={4}>Start Time:</Form.Label>
+                                <Col sm={8}>
+                                    <Form.Control type={"text"} placeholder={"Start Time"}
+                                                  value={this.state.startTime} onChange={this.changeHandler}
+                                                  name={"startTime"}/>
+                                </Col>
+                            </Form.Group>
+
+                            {/*<Form.Group as={Row} controlId={"score"}>*/}
+                            {/*    <Form.Label column sm={4}>Score:</Form.Label>*/}
+                            {/*    <Col sm={8}>*/}
+                            {/*        <Form.Control type={"text"} placeholder={"Score"}*/}
+                            {/*                      value={this.state.score} onChange={this.changeHandler}*/}
+                            {/*                      name={"score"}/>*/}
+                            {/*    </Col>*/}
                             {/*</Form.Group>*/}
 
-                            <Form.Group controlId={"instructorId"}>
-                                <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.instructorId}
-                                              onChange={this.changeHandler} name={"instructorId"}>
-                                    <option value="empty"> </option>
-                                    {this.state.users.map(user => <option value={user.id}>
-                                        {user.firstName + " " + user.lastName}</option>)}
-                                    {/*<option value="PENDING">Pending</option>*/}
-                                    {/*<option value="APPROVED">Approved</option>*/}
-                                    {/*<option value="DECLINED">Declined</option>*/}
-                                    {/*<option value="CANCELLED">Cancelled</option>*/}
-                                    {/*<option value="COMPLETE">Completed</option>*/}
-                                </Form.Control>
-                            </Form.Group>
+                            {/*<Form.Group as={Row} controlId={"comments"}>*/}
+                            {/*    <Form.Label column sm={4}>Comments:</Form.Label>*/}
+                            {/*    <Col sm={8}>*/}
+                            {/*        <Form.Control type={"text"} placeholder={"Comments"}*/}
+                            {/*                      value={this.state.comments} onChange={this.changeHandler}*/}
+                            {/*                      name={"comments"}/>*/}
+                            {/*    </Col>*/}
+                            {/*</Form.Group>*/}
 
-                            <Form.Group controlId={"studentId"}>
-                                <Form.Control type={"text"} placeholder={"Student ID"}
-                                              value={this.state.studentId} onChange={this.changeHandler}
-                                              name={"studentId"}/>
-                            </Form.Group>
-
-                            <Form.Group controlId={"date"}>
-                                <DatePicker
-                                    selected={this.state.date}
-                                    onChange={this.handleDateChange}
-                                    name="date"
-                                    dateFormat="MM/dd/yyyy"
-                                />
-                            </Form.Group>
-
-                            <Form.Group controlId={"startTime"}>
-                                <Form.Control type={"text"} placeholder={"Start Time"}
-                                              value={this.state.startTime} onChange={this.changeHandler}
-                                              name={"startTime"}/>
-                            </Form.Group>
-
-                            <Form.Group controlId={"endTime"}>
-                                <Form.Control type={"text"} placeholder={"End Time"}
-                                              value={this.state.endTime} onChange={this.changeHandler}
-                                              name={"endTime"}/>
-                            </Form.Group>
-
-                            <Form.Group controlId={"score"}>
-                                <Form.Control type={"text"} placeholder={"Score"}
-                                              value={this.state.score} onChange={this.changeHandler}
-                                              name={"score"}/>
-                            </Form.Group>
-
-                            <Form.Group controlId={"comments"}>
-                                <Form.Control type={"text"} placeholder={"Comments"}
-                                              value={this.state.comments} onChange={this.changeHandler}
-                                              name={"comments"}/>
-                            </Form.Group>
-
-                            <Form.Group controlId={"state"}>
-                                <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.state}
-                                              onChange={this.changeHandler} name={"state"}>
-                                    <option value="empty"> </option>
-                                    <option value="PENDING">Pending</option>
-                                    <option value="APPROVED">Approved</option>
-                                    <option value="DECLINED">Declined</option>
-                                    <option value="CANCELLED">Cancelled</option>
-                                    <option value="COMPLETE">Completed</option>
-                                </Form.Control>
+                            <Form.Group as={Row} controlId={"state"}>
+                                <Form.Label column sm={4}>State:</Form.Label>
+                                <Col sm={8}>
+                                    <Form.Control as={"select"} className={"mr-sm-2"} value={this.state.state}
+                                                  onChange={this.changeHandler} name={"state"} readOnly plaintext>
+                                        <option value="PENDING">Pending</option>
+                                    </Form.Control>
+                                </Col>
                             </Form.Group>
 
                             <Button variant="dark" type="submit" onClick={this.submitHandler}>Submit</Button>
