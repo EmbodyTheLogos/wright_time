0d64f6767e6978657ae63cba870bc075f6a8d824
rustylocks79 <jeremydellock570@gmail.com>
2020-12-08 20:17:27 -0500

    getCertificationByUser.

--- src/main/java/edu/team5/wright_time/controller/CertificationController.java
+++ src/main/java/edu/team5/wright_time/controller/CertificationController.java
@@ -2,11 +2,13 @@ package edu.team5.wright_time.controller;
 
 import edu.team5.wright_time.model.entity.Certification;
 import edu.team5.wright_time.model.repository.CertificationRepository;
+import edu.team5.wright_time.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -14,10 +16,12 @@ import java.util.NoSuchElementException;
 @CrossOrigin(origins = "http://localhost:3000")
 public class CertificationController {
     private final CertificationRepository certificationRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CertificationController(CertificationRepository certificationRepository) {
+    public CertificationController(CertificationRepository certificationRepository, UserRepository userRepository) {
         this.certificationRepository = certificationRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
@@ -30,6 +34,13 @@ public class CertificationController {
     @Secured({ "ROLE_STUDENT", "ROLE_INSTRUCTOR", "ROLE_ADMIN" })
     public Certification getOneCertification(@PathVariable long id) throws NoSuchElementException {
         return certificationRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No certification with id: " + id));
+    }
+
+    @GetMapping("user/{id}")
+    @Secured({ "ROLE_STUDENT", "ROLE_INSTRUCTOR", "ROLE_ADMIN" })
+    public List<Certification> getCertificationByUser(@PathVariable long id) throws NoSuchElementException {
+        final var user = this.userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user with id: " + id));
+        return certificationRepository.findCertificationByUser(user);
     }
 
     @PostMapping
--- src/main/java/edu/team5/wright_time/model/repository/CertificationRepository.java
+++ src/main/java/edu/team5/wright_time/model/repository/CertificationRepository.java
@@ -1,12 +1,14 @@
 package edu.team5.wright_time.model.repository;
 
-import edu.team5.wright_time.model.entity.Certification;
-import org.springframework.data.repository.CrudRepository;
 import edu.team5.wright_time.model.entity.Aircraft;
+import edu.team5.wright_time.model.entity.Certification;
+import edu.team5.wright_time.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CertificationRepository extends CrudRepository <Certification, Long> {
 
     List<Certification> findCertificationByAircraft(Aircraft aircraft);
+    List<Certification> findCertificationByUser(User user);
 }
