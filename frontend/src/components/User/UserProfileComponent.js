ba2df179f6e840b66ffe2a2685c61ad3de8583dd
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-12-09 12:53:09 -0500

    updated profile page

--- frontend/src/components/User/UserProfileComponent.js
+++ frontend/src/components/User/UserProfileComponent.js
@@ -5,6 +5,7 @@ import {withRouter} from "react-router-dom";
 import AuthService from "../../services/AuthService";
 import UserService from "../../services/UserService";
 import CertificationService from "../../services/CertificationService";
+import {Col, Container, Row} from "react-bootstrap";
 
 class UserProfileComponent extends React.Component {
     state = {
@@ -20,7 +21,7 @@ class UserProfileComponent extends React.Component {
         this.state.jwtToken = cookies.get('JWT-TOKEN')
     }
 
-    componentDidMount(){
+    componentDidMount() {
         AuthService.user(this.state.jwtToken).then((res) => {
             this.setState({user: res.data})
             UserService.getTotalHours(this.state.jwtToken, this.state.user.id).then((res) => {
@@ -38,14 +39,39 @@ class UserProfileComponent extends React.Component {
                 <UserNavbar/>
                 <br/>
 
-                <h3>Profile of {this.state.user.firstName + " " + this.state.user.lastName}</h3>
-                <br/>
-                <br/>
-                <h5>Contact at: {this.state.user.email}</h5>
-                <br/>
-                <h5>Born: {this.state.user.dateOfBirth}</h5>
-                <br/>
-                <h5>{this.state.hours} total flight hours</h5>
+                <h3 style={{textAlign: "left", marginLeft: "50px"}}>Profile
+                    of {this.state.user.firstName + " " + this.state.user.lastName}</h3>
+                <h5 style={{textAlign: "left", marginLeft: "50px"}}>Contact at: {this.state.user.email}</h5>
+
+                <Container>
+                    <Row>
+                        <Col sm={6}>
+                            <h5 style={{textAlign: "right"}}>Role:</h5>
+                        </Col>
+                        <Col sm={6}>
+                            {this.state.user.role === "ROLE_ADMIN" && <h5 style={{textAlign: "left"}}>Admin</h5>}
+                            {this.state.user.role === "ROLE_INSTRUCTOR" &&
+                            <h5 style={{textAlign: "left"}}>Instructor</h5>}
+                            {this.state.user.role === "ROLE_STUDENT" && <h5 style={{textAlign: "left"}}>Student</h5>}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={6}>
+                            <h5 style={{textAlign: "right"}}>Born on: </h5>
+                        </Col>
+                        <Col sm={6}>
+                            <h5 style={{textAlign: "left"}}>{this.state.user.dateOfBirth}</h5>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={6}>
+                            <h5 style={{textAlign: "right"}}>Total flight time:</h5>
+                        </Col>
+                        <Col sm={6}>
+                            <h5 style={{textAlign: "left"}}>{this.state.hours} Hours</h5>
+                        </Col>
+                    </Row>
+                </Container>
                 <br/>
                 <h5>Aircraft You Are Certified to Fly: </h5>
                 {
@@ -53,6 +79,7 @@ class UserProfileComponent extends React.Component {
                         <h5>{cert.aircraft.manufacturer + ' ' + cert.aircraft.name + ' ' + cert.aircraft.model}</h5>
                     )
                 }
+
 
             </div>
         )
