ebfde01d95c667b5efeb962b28d53b213ec275f6
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-12-09 15:54:37 -0500

    updated more components with new visuals

--- frontend/src/components/Administrator/AircraftComponent.js
+++ frontend/src/components/Administrator/AircraftComponent.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import AircraftService from '../../services/AircraftService';
 import {Link, withRouter} from "react-router-dom";
-import {Button} from 'react-bootstrap'
+import {Button, Container} from 'react-bootstrap'
 import AdministratorNavbar from "../Navbars/AdministratorNavbar";
 import {withCookies} from "react-cookie";
 import AuthService from "../../services/AuthService";
 import UserNavbar from "../Navbars/UserNavbar";
+import BGImage from "../../Images/cloudy_sky.jpg"
+
+var bg = {
+    backgroundImage: `url(${BGImage})`,
+    display: 'flex',
+    height: '100vh',
+    // borderStyle: 'solid',
+    // borderColor: 'yellow',
+}
+
+var content = {
+    backgroundColor: 'white',
+    margin: '10px auto',
+    paddingTop: '20px',
+    paddingRight: '30px',
+    paddingLeft: '30px',
+    paddingBottom: '20px',
+    // borderStyle: 'solid',
+    // borderColor: 'red',
+    overflow: 'auto',
+}
 
 class AircraftComponent extends React.Component {
     state = {
@@ -14,87 +35,92 @@ class AircraftComponent extends React.Component {
         user: ""
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
         const {cookies} = props;
         this.state.jwtToken = cookies.get('JWT-TOKEN')
     }
 
-    componentDidMount(){
+    componentDidMount() {
         AuthService.user(this.state.jwtToken).then((res) => {
             this.setState({user: res.data})
             console.log(this.state.user)
         })
 
         AircraftService.getAll(this.state.jwtToken).then((response) => {
-            this.setState({ aircrafts: response.data})
+            this.setState({aircrafts: response.data})
         });
     }
 
-    render (){
+    render() {
         const role = this.state.user.role
         return (
             <div>
                 {role === "ROLE_ADMIN" ? <AdministratorNavbar/> : <UserNavbar/>}
+                <div style={bg}>
+                    <Container style={content}>
+                        <h1>Aircraft List</h1>
+                        <div className="container mt-4">
+                            <table className="table table-bordered table-hover">
+                                <thead className="thead-dark">
+                                <tr>
+                                    <th scope={"col"}> ID Number</th>
+                                    <th scope={"col"}> Manufacturer</th>
+                                    <th scope={"col"}> Name</th>
+                                    <th scope={"col"}> Model</th>
+                                    <th scope={"col"}> Year</th>
+                                    <th scope={"col"}> Maintenance Day</th>
+                                    <th scope={"col"}> Training Duration</th>
+                                    {role === "ROLE_ADMIN" && <th scope={"col"}></th>}
+                                    {role === "ROLE_ADMIN" && <th scope={"col"}></th>}
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.aircrafts.map(
+                                        aircraft =>
+                                            <tr key={aircraft.id}>
+                                                <th scope={"row"}> {aircraft.id}</th>
+                                                <td> {aircraft.manufacturer}</td>
+                                                <td> {aircraft.name}</td>
+                                                <td> {aircraft.model}</td>
+                                                <td> {aircraft.year}</td>
+                                                <td> {aircraft.maintenanceDay}</td>
+                                                <td> {aircraft.trainingDuration}</td>
+                                                {role === "ROLE_ADMIN" &&
+                                                <td>
+                                                    <Link to={"/admin/aircraft/edit/" + aircraft.id}
+                                                          className={"btn btn-warning btn-block"}>Edit Aircraft</Link>
+                                                </td>
+                                                }
+                                                {role === "ROLE_ADMIN" &&
+                                                <td>
+                                                    <Button variant={"danger"}
+                                                            onClick={() => {
+                                                                AircraftService.delete(this.state.jwtToken, aircraft.id);
+                                                                window.location.reload(false);
+                                                            }}>
+                                                        Delete
+                                                    </Button>
+                                                </td>
+                                                }
+                                            </tr>
+                                    )
+                                }
+                                </tbody>
+                            </table>
 
-                <h1>Aircraft List</h1>
-                <div className="container mt-4">
-                    <table className="table table-bordered table-hover">
-                        <thead className="thead-dark">
-                        <tr>
-                            <th scope={"col"}> ID Number</th>
-                            <th scope={"col"}> Manufacturer</th>
-                            <th scope={"col"}> Name</th>
-                            <th scope={"col"}> Model</th>
-                            <th scope={"col"}> Year</th>
-                            <th scope={"col"}> Maintenance Day</th>
-                            <th scope={"col"}> Training Duration</th>
-                            {role === "ROLE_ADMIN" && <th scope={"col"}> </th>}
-                            {role === "ROLE_ADMIN" && <th scope={"col"}> </th>}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.aircrafts.map(
-                                aircraft =>
-                                    <tr key = {aircraft.id}>
-                                        <th scope={"row"}> {aircraft.id}</th>
-                                        <td> {aircraft.manufacturer}</td>
-                                        <td> {aircraft.name}</td>
-                                        <td> {aircraft.model}</td>
-                                        <td> {aircraft.year}</td>
-                                        <td> {aircraft.maintenanceDay}</td>
-                                        <td> {aircraft.trainingDuration}</td>
-                                        {role === "ROLE_ADMIN" &&
-                                            <td>
-                                                <Link to={"/admin/aircraft/edit/" + aircraft.id}
-                                                   className={"btn btn-warning btn-block"}>Edit Aircraft</Link>
-                                            </td>
-                                        }
-                                        {role === "ROLE_ADMIN" &&
-                                            <td>
-                                                <Button variant={"danger"}
-                                                        onClick={() => {
-                                                            AircraftService.delete(this.state.jwtToken, aircraft.id);
-                                                            window.location.reload(false);
-                                                        }}>
-                                                    Delete
-                                                </Button>
-                                            </td>
-                                        }
-                                    </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
+                            <br/>
+                            {role === "ROLE_ADMIN" &&
+                            <Link to={"/admin/aircraft/add"} className={"btn btn-dark"}>Add Aircraft</Link>}
 
-                    <br/>
-                    { role === "ROLE_ADMIN" && <Link to={"/admin/aircraft/add"} className={"btn btn-dark"}>Add Aircraft</Link>}
-
+                        </div>
+                    </Container>
                 </div>
 
             </div>
         )
     }
 }
+
 export default withCookies(withRouter(AircraftComponent))
--- frontend/src/components/Administrator/CertificationComponent.js
+++ frontend/src/components/Administrator/CertificationComponent.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import CertificationService from '../../services/CertificationService';
 import {Link, withRouter} from "react-router-dom";
-import {Button} from 'react-bootstrap'
+import {Button, Container} from 'react-bootstrap'
 import AdministratorNavbar from "../Navbars/AdministratorNavbar";
 import {withCookies} from "react-cookie";
+import BGImage from "../../Images/cloudy_sky.jpg"
+
+var bg = {
+    backgroundImage: `url(${BGImage})`,
+    display: 'flex',
+    height: '100vh',
+    // borderStyle: 'solid',
+    // borderColor: 'yellow',
+}
+
+var content = {
+    backgroundColor: 'white',
+    margin: '10px auto',
+    paddingTop: '20px',
+    paddingRight: '30px',
+    paddingLeft: '30px',
+    paddingBottom: '20px',
+    // borderStyle: 'solid',
+    // borderColor: 'red',
+    overflow: 'auto',
+}
 
 class CertificationComponent extends React.Component {
     state = {
@@ -11,69 +32,74 @@ class CertificationComponent extends React.Component {
         jwtToken: ""
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
         const {cookies} = props;
         this.state.jwtToken = cookies.get('JWT-TOKEN')
     }
 
-    componentDidMount(){
+    componentDidMount() {
         CertificationService.getAll(this.state.jwtToken).then((response) => {
-            this.setState({ certifications: response.data})
+            this.setState({certifications: response.data})
         });
     }
 
-    render (){
+    render() {
         return (
             <div>
                 <AdministratorNavbar/>
+                <div style={bg}>
+                    <Container style={content}>
+                        <h1>Certification List</h1>
+                        <div className="container mt-4">
+                            <table className="table table-bordered table-hover">
+                                <thead className="thead-dark">
+                                <tr>
+                                    <th scope={"col"} d> Certification ID</th>
+                                    <th scope={"col"}> User</th>
+                                    <th scope={"col"}> Aircraft</th>
+                                    <th scope={"col"}> Date Obtained</th>
+                                    <th scope={"col"}></th>
+                                    <th scope={"col"}></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.certifications.map(
+                                        cert =>
+                                            <tr key={cert.id}>
+                                                <th scope={"row"}> {cert.id}</th>
+                                                <td> {cert.user.firstName + ' ' + cert.user.lastName}</td>
+                                                <td> {cert.aircraft.manufacturer + " " + cert.aircraft.model + " " + cert.aircraft.name}</td>
+                                                <td> {cert.dateObtained}</td>
+                                                <td>
+                                                    <Link to={"/admin/certifications/edit/" + cert.id}
+                                                          className={"btn btn-warning btn-block"}>Edit
+                                                        Certification</Link>
+                                                </td>
+                                                <td>
+                                                    <Button variant={"danger"}
+                                                            onClick={() => {
+                                                                CertificationService.delete(this.state.jwtToken, cert.id);
+                                                                window.location.reload(false);
+                                                            }}>
+                                                        Delete
+                                                    </Button>
+                                                </td>
+                                            </tr>
+                                    )
+                                }
+                                </tbody>
+                            </table>
 
-                <h1>Certification List</h1>
-                <div className="container mt-4">
-                    <table className="table table-bordered table-hover">
-                        <thead className="thead-dark">
-                        <tr>
-                            <th scope={"col"}d> Certification ID</th>
-                            <th scope={"col"}> User</th>
-                            <th scope={"col"}> Aircraft</th>
-                            <th scope={"col"}> Date Obtained</th>
-                            <th scope={"col"}> </th>
-                            <th scope={"col"}> </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.certifications.map(
-                                cert =>
-                                    <tr key = {cert.id}>
-                                        <th scope={"row"}> {cert.id}</th>
-                                        <td> {cert.user.firstName + ' ' + cert.user.lastName}</td>
-                                        <td> {cert.aircraft.manufacturer + " " + cert.aircraft.model + " " + cert.aircraft.name}</td>
-                                        <td> {cert.dateObtained}</td>
-                                        <td>
-                                            <Link to={"/admin/certifications/edit/" + cert.id}
-                                               className={"btn btn-warning btn-block"}>Edit Certification</Link>
-                                        </td>
-                                        <td>
-                                            <Button variant={"danger"}
-                                               onClick={() => {
-                                                   CertificationService.delete(this.state.jwtToken, cert.id);
-                                                   window.location.reload(false);
-                                               }}>
-                                                Delete
-                                            </Button>
-                                        </td>
-                                    </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-
-                    <br/>
-                    <Link to={"/admin/certifications/add"} className={"btn btn-dark"}>Add Certification</Link>
+                            <br/>
+                            <Link to={"/admin/certifications/add"} className={"btn btn-dark"}>Add Certification</Link>
+                        </div>
+                    </Container>
                 </div>
             </div>
         )
     }
 }
+
 export default withCookies(withRouter(CertificationComponent))
